8c15648676a3ad2bb736c70e0130a251ce272742
Laurianne <valette.laurianne@gmail.com>
2022-10-25 16:13:27 +0200

    Research from the main bar

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -3,9 +3,11 @@ import Recipe from '../factories/recipe.js';
 
 // DOM elements
 const resultsDOM = document.getElementById('results');
+const mainBar = document.getElementById('main-bar');
 
 // Variables declarations
 let results = Object.values(recipes);
+let userSearch
 
 // Display the recipes in the results area
 function displayRecipes() {
@@ -15,9 +17,48 @@ function displayRecipes() {
     })
 }
 
+// Search from the main bar in  the titles, ingredients and  description
+function searchRecipes() {
+    for (let i = 0; i < recipes.length; i++) {
+        let lcName = recipes[i].name.toLowerCase()
+        let lcIngredients
+        for (let j=0; j<recipes[i].ingredients.length; j++) {
+            lcIngredients += recipes[i].ingredients[j].ingredient.toLowerCase()
+        }
+        let lcDescription = recipes[i].description.toLowerCase()
+        if (lcName.includes(userSearch) || lcIngredients.includes(userSearch) || lcDescription.includes(userSearch)) {
+            results.push(recipes[i]);
+        }
+    }
+}
+
+// Empty DOM in the results area
+function initializeResults() {
+    resultsDOM.innerHTML =  '';
+    results.length = 0;
+}
+
+// Listen inputs in the main bar and trigger the corresponding actions
+function updateResults() {
+    mainBar.addEventListener('input', () =>  {
+        userSearch = mainBar.value.toLowerCase();
+
+        if (userSearch.length > 2) {
+            initializeResults()
+            searchRecipes();
+            displayRecipes();
+        } else {
+            initializeResults()
+            results = Object.values(recipes);
+            displayRecipes();
+        }
+    })
+}
+
 // Main function
 function init() {
     displayRecipes()
+    updateResults()
 }
 
 init()
